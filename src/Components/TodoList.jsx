b65e83e5cfd5c047c8f12be1216dcc9b8f0366ce
Joel <joelramonph@gmail.com>
2025-01-16 11:22:40 -0400

    add funcionalidad agregar titulo de tarea y renderizar

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -3,22 +3,45 @@ import { useState } from "react";
 
 
 function TodoList() {
-    
-    return(
-        <> 
-        <div className="container">
-            <h1>TODO LIST</h1>
-            <div className="input-container">
-                <input type="text"
-                className="heading-input"
-                placeholder="Ingrese Tarea"
-                />
-            <button className="add-list-button">Agregar Lista Tarea</button>
+    const [todos, setTodos] = useState([])
+    const [headingInput, setHeadingInput] = useState('');
+    const [listInput, setListInput] = useState({})
+
+    // funcionalidad agregar titulo de la tarea
+
+    const handleAddTodo = () => {
+        if (headingInput.trim() !== "") {
+            setTodos([...todos, { heading: headingInput, list: [] }])
+        }
+    }
+
+    return (
+        <>
+            <div className="container">
+                <h1 className="title">TODO LIST</h1>
+                <div className="input-container">
+                    <input type="text"
+                        className="heading-input"
+                        placeholder="Ingrese Titulo de Tarea"
+                        value={headingInput}
+                        onChange={(e) => { setHeadingInput(e.target.value); }}
+                    />
+                    <button className="add-list-button" onClick={handleAddTodo}>Agregar Lista Tarea</button>
+                </div>
+
+                <div className="todo-main">
+                    {todos.map((todo, index) => (
+                        <div key={index} className="todo-card">
+                            <div className="heading_todo">
+                                <h3>{todo.heading}</h3> 
+                                <button className="delete-button-heading" onClick={() => handleDeleteTodo(index)}>Delete Heading </button>
+                            </div>
+                        </div>
+                    )
+                    )}
+                </div>
             </div>
 
-            <div className="todo-main"></div>
-        </div>
-        
         </>
     )
 }
